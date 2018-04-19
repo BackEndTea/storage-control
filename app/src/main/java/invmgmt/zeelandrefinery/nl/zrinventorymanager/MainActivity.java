763f67d8595297ec6eb00f50e2182b1f89c31ea9
juliansteenbakker <juliansteenbakker@outlook.com>
2018-04-19 21:41:42 +0200

    Add drawer layout Add first fragment

--- app/src/main/java/invmgmt/zeelandrefinery/nl/zrinventorymanager/MainActivity.java
+++ app/src/main/java/invmgmt/zeelandrefinery/nl/zrinventorymanager/MainActivity.java
@@ -1,74 +1,141 @@
 package invmgmt.zeelandrefinery.nl.zrinventorymanager;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawer;
-    private Toolbar toolbar;
-    private NavigationView nvDrawer;
 
-    // Make sure to be using android.support.v7.app.ActionBarDrawerToggle version.
-    // The android.support.v4.app.ActionBarDrawerToggle has been deprecated.
-    private ActionBarDrawerToggle drawerToggle;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private MenuItem prevMenuItem;
+    public static FragmentManager fragmentManager;
+
+    NotificationManager mNotifyMgr;
+    NavigationView nvDrawer;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null) {
+            fragmentManager = getSupportFragmentManager();
+            initViews();
+            Log.d("Intent", "Start nieuwe service");
+
+            FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+            tx.replace(R.id.flContent, new MainFragment());
+            tx.commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //RelativeLayout layout = (RelativeLayout) findViewById(R.layout.activity_home);
+
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    private void initViews() {
         // Set a Toolbar to replace the ActionBar.
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // Attaching the layout to the toolbar object
 
         // Find our drawer view
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = setupDrawerToggle();
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
 
-        // Tie DrawerLayout events to the ActionBarToggle
-        mDrawer.addDrawerListener(drawerToggle);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        // Find our drawer view
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Set Toolbar
+        if (toolbar != null) {
+            toolbar.setLogo(R.mipmap.ic_launcher);
+        }
+        setSupportActionBar(toolbar); // Setting toolbar as the ActionBar with setSupportActionBar()
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        }
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                // invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //  invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        nvDrawer.getMenu().getItem(0).setChecked(true);
     }
 
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
-        // and will not render the hamburger icon without it.
-        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    // `onPostCreate` called when activity start-up is complete after `onStart()`
-    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
-    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
-    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
+    // Make sure this is the method with just `Bundle` as the signature
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
+
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         // Pass any configuration change to the drawer toggles
-        drawerToggle.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
+    /**
+     * Setup Fragments
+     */
 
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -82,14 +149,22 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+
     public void selectDrawerItem(MenuItem menuItem) {
-        // Create a new fragment and specify the fragment to show based on nav item clicked
+        // Create a new fragment and specify the planet to show based on
+        // position
         Fragment fragment = null;
         Class fragmentClass;
-        switch(menuItem.getItemId()) {
+
+        //Checking if the item is in checked state or not, if not make it in checked state
+
+        menuItem.setChecked(true);
+        switch (menuItem.getItemId()) {
             case R.id.nav_first_fragment:
                 fragmentClass = MainFragment.class;
                 break;
+
+
             default:
                 fragmentClass = MainFragment.class;
         }
@@ -104,11 +179,46 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
 
-        // Highlight the selected item has been done by NavigationView
-        menuItem.setChecked(true);
-        // Set action bar title
-        setTitle(menuItem.getTitle());
-        // Close the navigation drawer
-        mDrawer.closeDrawers();
+        // Highlight the selected item, update the title, and close the drawer
+        if (prevMenuItem != null) {
+            prevMenuItem.setChecked(false);
+        } else {
+            nvDrawer.getMenu().getItem(0).setChecked(false);
+        }
+        mDrawerLayout.closeDrawers();
+        prevMenuItem = menuItem;
+        mDrawerLayout.closeDrawers();
     }
+
+
+    @Override
+    public void onDestroy() {
+        mNotifyMgr =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotifyMgr.cancelAll();
+        super.onDestroy();
+
+    }
+
+    private Boolean exit = false;
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, R.string.backpressed,
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
+    }
+
+
 }
